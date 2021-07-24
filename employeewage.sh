5d82c7ff4cb5c0e7eb764660f67e777b575106b4
rojajullapalli <rojajules@gmail.com>
2021-07-24 19:46:44 +0530

    calculating wages for month

--- employeewage.sh
+++ employeewage.sh
@@ -3,17 +3,22 @@ echo "Welcome to the employee wage page"
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$(( RANDOM%3 ));
+numWorkingDays=20;
 
-case $randomCheck in
-	$isFullTime)
-					empHrs=8
-					;;
-	$isPartTime)
-					empHrs=4
-					;;
-	*)
-					empHrs=0
-					;;
-esac
-salary=$(( $empHrs*$empRatePerHr ));
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$(( RANDOM%3 ));
+		case $empCheck in
+			$isFullTime)
+				empHrs=8
+				;;
+			$isPartTime)
+				empHrs=4
+				;;
+			*)
+			empHrs=0
+				;;
+		esac
+		salary=$(($empHrs*$empRatePerHr));
+		totalSalary=$(($totalSalary+$salary));
+done
