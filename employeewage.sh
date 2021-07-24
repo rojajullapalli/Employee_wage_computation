667118bd27e084a39a069438142a8eb45f1a21d9
rojajullapalli <rojajules@gmail.com>
2021-07-24 19:20:39 +0530

    employee is present or not

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
-echo "welcome to employee wage program"
+
+echo "Welcome to Employee Wage Computation program "
+ 
+# create random function and check employee is present or not using if else
+random=$(( RANDOM%2 ))
+if [ $random -eq 1 ]
+then
+	echo "Employee is present "
+else
+	echo "Employee is not present "
+fi
