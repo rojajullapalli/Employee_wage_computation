fa0ef61d14192fd751333a708d3d1194149b6357
rojajullapalli <rojajules@gmail.com>
2021-07-24 19:41:45 +0530

    using switch case

--- employeewage.sh
+++ employeewage.sh
@@ -4,13 +4,16 @@ isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
 randomCheck=$(( RANDOM%3 ));
-if [ $isFullTime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
+
+case $randomCheck in
+	$isFullTime)
+					empHrs=8
+					;;
+	$isPartTime)
+					empHrs=4
+					;;
+	*)
+					empHrs=0
+					;;
+esac
 salary=$(( $empHrs*$empRatePerHr ));
