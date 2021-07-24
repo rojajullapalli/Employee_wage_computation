8862be43a07ea65680d2bda293bf2e956ffc7748
rojajullapalli <rojajules@gmail.com>
2021-07-24 19:52:35 +0530

    calculate wages till condition of total working hours or days reached for a month

--- employeewage.sh
+++ employeewage.sh
@@ -1,24 +1,30 @@
 #!/bin/bash -x
 echo "Welcome to the employee wage page"
+
 isPartTime=1;
 isFullTime=2;
-empRatePerHr=20;
-numWorkingDays=20;
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
 do
-	empCheck=$(( RANDOM%3 ));
-		case $empCheck in
-			$isFullTime)
-				empHrs=8
-				;;
-			$isPartTime)
-				empHrs=4
-				;;
-			*)
+	totalWorkingDays=$(($totalWorkingDays+1))
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$isPartTime)
+			empHrs=4
+			;;
+		$isFullTime)
+			empHrs=8
+			;;
+		*)
 			empHrs=0
-				;;
-		esac
-		salary=$(($empHrs*$empRatePerHr));
-		totalSalary=$(($totalSalary+$salary));
+			;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
 done
